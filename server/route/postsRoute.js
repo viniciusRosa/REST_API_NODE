4b341aa33312e2d11a3a4ed1c57c35b657fa1a9b
Vinicius Rosa <vinicius.rosa00@gmail.com>
2021-01-27 17:20:11 -0300

    first commit

---
+++ server/route/postsRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const postsService = require('../service/postService')
+
+router.get('/posts', async function(req, res) {
+    const posts = await postsService.getPosts();
+    res.json(posts);
+});
+router.get('/posts/:id', async function(req, res) {
+
+});
+router.post('/posts', async function(req, res) {
+
+});
+router.put('/posts/:id', async function(req, res) {
+
+});
+router.delete('/posts/:id', async function(req, res) {
+
+});
+
+module.exports = router;
